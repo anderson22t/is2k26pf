fd3dc26ea89490a26a62a225db0ee5eff9e736b4
vennusamery <vennussalguero@gmail.com>
2026-04-11 21:40:23 -0600

    le puse el logo

--- codigo/empresarial/MRP - G1/DLLS/MVC_Dispo_Inv/MVC_Dispo_Inve/Capa_Vista/Frm_Disponibilidad_Inventario.Designer.cs
+++ codigo/empresarial/MRP - G1/DLLS/MVC_Dispo_Inv/MVC_Dispo_Inve/Capa_Vista/Frm_Disponibilidad_Inventario.Designer.cs
@@ -38,74 +38,80 @@ namespace Capa_Vista
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvInventario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvInventario
             // 
             this.dgvInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvInventario.Location = new System.Drawing.Point(22, 119);
+            this.dgvInventario.Location = new System.Drawing.Point(22, 179);
             this.dgvInventario.Name = "dgvInventario";
             this.dgvInventario.RowHeadersWidth = 51;
             this.dgvInventario.RowTemplate.Height = 24;
-            this.dgvInventario.Size = new System.Drawing.Size(704, 319);
+            this.dgvInventario.Size = new System.Drawing.Size(704, 259);
             this.dgvInventario.TabIndex = 0;
             // 
             // cmbTipo
             // 
             this.cmbTipo.FormattingEnabled = true;
-            this.cmbTipo.Location = new System.Drawing.Point(84, 72);
+            this.cmbTipo.Location = new System.Drawing.Point(94, 128);
             this.cmbTipo.Name = "cmbTipo";
-            this.cmbTipo.Size = new System.Drawing.Size(121, 24);
+            this.cmbTipo.Size = new System.Drawing.Size(128, 24);
             this.cmbTipo.TabIndex = 1;
             // 
             // cmbAlmacen
             // 
             this.cmbAlmacen.FormattingEnabled = true;
-            this.cmbAlmacen.Location = new System.Drawing.Point(327, 72);
+            this.cmbAlmacen.Location = new System.Drawing.Point(327, 128);
             this.cmbAlmacen.Name = "cmbAlmacen";
-            this.cmbAlmacen.Size = new System.Drawing.Size(121, 24);
+            this.cmbAlmacen.Size = new System.Drawing.Size(141, 24);
             this.cmbAlmacen.TabIndex = 2;
             // 
             // txtBusqueda
             // 
-            this.txtBusqueda.Location = new System.Drawing.Point(81, 44);
+            this.txtBusqueda.Location = new System.Drawing.Point(94, 90);
             this.txtBusqueda.Name = "txtBusqueda";
-            this.txtBusqueda.Size = new System.Drawing.Size(602, 22);
+            this.txtBusqueda.Size = new System.Drawing.Size(589, 22);
             this.txtBusqueda.TabIndex = 3;
             // 
             // lblTotal
             // 
             this.lblTotal.AutoSize = true;
-            this.lblTotal.Location = new System.Drawing.Point(585, 75);
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(585, 131);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(46, 17);
+            this.lblTotal.Size = new System.Drawing.Size(52, 17);
             this.lblTotal.TabIndex = 4;
             this.lblTotal.Text = "label1";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 79);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 135);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.Size = new System.Drawing.Size(45, 17);
             this.label1.TabIndex = 5;
             this.label1.Text = "Tipo:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(242, 79);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(242, 135);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.Size = new System.Drawing.Size(74, 17);
             this.label2.TabIndex = 6;
             this.label2.Text = "Almacen;";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(600, 75);
+            this.label3.Location = new System.Drawing.Point(600, 131);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 17);
             this.label3.TabIndex = 7;
@@ -113,19 +119,40 @@ namespace Capa_Vista
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(19, 47);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 95);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.Size = new System.Drawing.Size(63, 17);
             this.label4.TabIndex = 8;
             this.label4.Text = "Buscar:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(154, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(466, 39);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Disponibilidad de Inventario";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Capa_Vista.Properties.Resources.LOGO_MRP;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
             this.button1.BackgroundImage = global::Capa_Vista.Properties.Resources.buscar__1_;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.Location = new System.Drawing.Point(689, 39);
+            this.button1.Location = new System.Drawing.Point(689, 86);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(37, 31);
+            this.button1.Size = new System.Drawing.Size(49, 35);
             this.button1.TabIndex = 9;
             this.button1.UseVisualStyleBackColor = true;
             // 
@@ -134,6 +161,8 @@ namespace Capa_Vista
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(750, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -148,6 +177,7 @@ namespace Capa_Vista
             this.Text = "Frm_Disponibilidad_Inventario";
             this.Load += new System.EventHandler(this.Frm_Disponibilidad_Inventario_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvInventario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,5 +195,7 @@ namespace Capa_Vista
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
