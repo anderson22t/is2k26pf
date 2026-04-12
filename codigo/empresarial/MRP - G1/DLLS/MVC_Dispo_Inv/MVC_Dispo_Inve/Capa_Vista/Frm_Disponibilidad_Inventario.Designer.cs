ea3a2a480c7a3e481a9c571370dd8de9fd2d6f52
vennusamery <vennussalguero@gmail.com>
2026-04-11 21:17:34 -0600

    ni tan funcional ni tan no funcional, olvide crear otra rama creo upsi ni modo seguimos en la de tipo material porque tengo sueño auida no puedo usar el navegador transaccional mi proceso esta feo porque me dieron este

---
+++ codigo/empresarial/MRP - G1/DLLS/MVC_Dispo_Inv/MVC_Dispo_Inve/Capa_Vista/Frm_Disponibilidad_Inventario.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Capa_Vista
+{
+    partial class Frm_Disponibilidad_Inventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvInventario = new System.Windows.Forms.DataGridView();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.cmbAlmacen = new System.Windows.Forms.ComboBox();
+            this.txtBusqueda = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventario)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvInventario
+            // 
+            this.dgvInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInventario.Location = new System.Drawing.Point(22, 119);
+            this.dgvInventario.Name = "dgvInventario";
+            this.dgvInventario.RowHeadersWidth = 51;
+            this.dgvInventario.RowTemplate.Height = 24;
+            this.dgvInventario.Size = new System.Drawing.Size(704, 319);
+            this.dgvInventario.TabIndex = 0;
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Location = new System.Drawing.Point(84, 72);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(121, 24);
+            this.cmbTipo.TabIndex = 1;
+            // 
+            // cmbAlmacen
+            // 
+            this.cmbAlmacen.FormattingEnabled = true;
+            this.cmbAlmacen.Location = new System.Drawing.Point(327, 72);
+            this.cmbAlmacen.Name = "cmbAlmacen";
+            this.cmbAlmacen.Size = new System.Drawing.Size(121, 24);
+            this.cmbAlmacen.TabIndex = 2;
+            // 
+            // txtBusqueda
+            // 
+            this.txtBusqueda.Location = new System.Drawing.Point(81, 44);
+            this.txtBusqueda.Name = "txtBusqueda";
+            this.txtBusqueda.Size = new System.Drawing.Size(645, 22);
+            this.txtBusqueda.TabIndex = 3;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(680, 75);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(46, 17);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tipo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Almacen;";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(600, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Total:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Buscar:";
+            // 
+            // Frm_Disponibilidad_Inventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.txtBusqueda);
+            this.Controls.Add(this.cmbAlmacen);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.dgvInventario);
+            this.Name = "Frm_Disponibilidad_Inventario";
+            this.Text = "Frm_Disponibilidad_Inventario";
+            this.Load += new System.EventHandler(this.Frm_Disponibilidad_Inventario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventario)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvInventario;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.ComboBox cmbAlmacen;
+        private System.Windows.Forms.TextBox txtBusqueda;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
