05c13b9bbb2b0538674abba5a9eab988d2f5974e
VennusAmery <vennussalguero@gmail.com>
2026-04-23 18:13:14 -0600

    ajustes de estandarizacion y reporte

--- codigo/empresarial/MRP - G1/DLLS/MVC_Dispo_Inv/MVC_DispoInve2/MVC__DispoInve/Capa_Vista_DispoInve/Frm_DispoInve.Designer.cs
+++ codigo/empresarial/MRP - G1/DLLS/MVC_Dispo_Inv/MVC_DispoInve2/MVC__DispoInve/Capa_Vista_DispoInve/Frm_DispoInve.Designer.cs
@@ -34,11 +34,21 @@ namespace Capa_Vista_DispoInve
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.lblTotal = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.dgvInventario = new System.Windows.Forms.DataGridView();
             this.txtBusqueda = new System.Windows.Forms.TextBox();
             this.cmbAlmacen = new System.Windows.Forms.ComboBox();
             this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_ayuda = new System.Windows.Forms.Button();
+            this.btn_imprimir = new System.Windows.Forms.Button();
+            this.btn_actualizar = new System.Windows.Forms.Button();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_editar = new System.Windows.Forms.Button();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvInventario)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,99 +56,209 @@ namespace Capa_Vista_DispoInve
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Rockwell", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(67, 31);
+            this.label1.Location = new System.Drawing.Point(38, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(495, 41);
+            this.label1.Size = new System.Drawing.Size(593, 41);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Disponibilidad De Inventario";
+            this.label1.Text = "DISPONIBILIDAD DE INVENTARIO";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Rockwell", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(67, 102);
+            this.label2.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 104);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 18);
+            this.label2.Size = new System.Drawing.Size(86, 24);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Buscar:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Rockwell", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(67, 148);
+            this.label3.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 135);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 18);
+            this.label3.Size = new System.Drawing.Size(62, 24);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Tipo:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Rockwell", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(288, 148);
+            this.label4.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(406, 135);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 18);
+            this.label4.Size = new System.Drawing.Size(104, 24);
             this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.label4.Text = "Almacen:";
             // 
             // lblTotal
             // 
             this.lblTotal.AutoSize = true;
-            this.lblTotal.Font = new System.Drawing.Font("Rockwell", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotal.Location = new System.Drawing.Point(669, 148);
+            this.lblTotal.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(32, 518);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(51, 18);
+            this.lblTotal.Size = new System.Drawing.Size(71, 24);
             this.lblTotal.TabIndex = 4;
             this.lblTotal.Text = "label5";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(660, 96);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(55, 34);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // dgvInventario
             // 
             this.dgvInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvInventario.Location = new System.Drawing.Point(70, 202);
+            this.dgvInventario.Location = new System.Drawing.Point(35, 229);
             this.dgvInventario.Name = "dgvInventario";
             this.dgvInventario.RowHeadersWidth = 51;
             this.dgvInventario.RowTemplate.Height = 24;
-            this.dgvInventario.Size = new System.Drawing.Size(655, 150);
+            this.dgvInventario.Size = new System.Drawing.Size(999, 276);
             this.dgvInventario.TabIndex = 6;
             // 
             // txtBusqueda
             // 
-            this.txtBusqueda.Location = new System.Drawing.Point(120, 102);
+            this.txtBusqueda.Location = new System.Drawing.Point(124, 104);
             this.txtBusqueda.Name = "txtBusqueda";
-            this.txtBusqueda.Size = new System.Drawing.Size(534, 22);
+            this.txtBusqueda.Size = new System.Drawing.Size(855, 22);
             this.txtBusqueda.TabIndex = 7;
             // 
             // cmbAlmacen
             // 
             this.cmbAlmacen.FormattingEnabled = true;
-            this.cmbAlmacen.Location = new System.Drawing.Point(357, 148);
+            this.cmbAlmacen.Location = new System.Drawing.Point(516, 135);
             this.cmbAlmacen.Name = "cmbAlmacen";
-            this.cmbAlmacen.Size = new System.Drawing.Size(121, 24);
+            this.cmbAlmacen.Size = new System.Drawing.Size(264, 24);
             this.cmbAlmacen.TabIndex = 10;
             // 
             // cmbTipo
             // 
             this.cmbTipo.FormattingEnabled = true;
-            this.cmbTipo.Location = new System.Drawing.Point(124, 144);
+            this.cmbTipo.Location = new System.Drawing.Point(124, 135);
             this.cmbTipo.Name = "cmbTipo";
-            this.cmbTipo.Size = new System.Drawing.Size(121, 24);
+            this.cmbTipo.Size = new System.Drawing.Size(264, 24);
             this.cmbTipo.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(35, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(999, 8);
+            this.label5.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(35, 218);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(999, 8);
+            this.label6.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(32, 185);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 24);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Resultados:";
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.BackgroundImage = global::Capa_Vista_DispoInve.Properties.Resources.salir;
+            this.btn_salir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_salir.Location = new System.Drawing.Point(985, 21);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(49, 41);
+            this.btn_salir.TabIndex = 18;
+            this.btn_salir.UseVisualStyleBackColor = true;
+            // 
+            // btn_ayuda
+            // 
+            this.btn_ayuda.BackgroundImage = global::Capa_Vista_DispoInve.Properties.Resources.ayuda;
+            this.btn_ayuda.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_ayuda.Location = new System.Drawing.Point(930, 21);
+            this.btn_ayuda.Name = "btn_ayuda";
+            this.btn_ayuda.Size = new System.Drawing.Size(49, 41);
+            this.btn_ayuda.TabIndex = 17;
+            this.btn_ayuda.UseVisualStyleBackColor = true;
+            // 
+            // btn_imprimir
+            // 
+            this.btn_imprimir.BackgroundImage = global::Capa_Vista_DispoInve.Properties.Resources.imprimir;
+            this.btn_imprimir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_imprimir.Location = new System.Drawing.Point(875, 21);
+            this.btn_imprimir.Name = "btn_imprimir";
+            this.btn_imprimir.Size = new System.Drawing.Size(49, 41);
+            this.btn_imprimir.TabIndex = 16;
+            this.btn_imprimir.UseVisualStyleBackColor = true;
+            this.btn_imprimir.Click += new System.EventHandler(this.btn_imprimir_Click);
+            // 
+            // btn_actualizar
+            // 
+            this.btn_actualizar.BackgroundImage = global::Capa_Vista_DispoInve.Properties.Resources.refrescar;
+            this.btn_actualizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_actualizar.Location = new System.Drawing.Point(820, 21);
+            this.btn_actualizar.Name = "btn_actualizar";
+            this.btn_actualizar.Size = new System.Drawing.Size(49, 41);
+            this.btn_actualizar.TabIndex = 15;
+            this.btn_actualizar.UseVisualStyleBackColor = true;
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.BackgroundImage = global::Capa_Vista_DispoInve.Properties.Resources.eliminar;
+            this.btn_eliminar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_eliminar.Location = new System.Drawing.Point(765, 21);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(49, 41);
+            this.btn_eliminar.TabIndex = 14;
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            // 
+            // btn_editar
+            // 
+            this.btn_editar.BackgroundImage = global::Capa_Vista_DispoInve.Properties.Resources.modificar;
+            this.btn_editar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_editar.Location = new System.Drawing.Point(710, 21);
+            this.btn_editar.Name = "btn_editar";
+            this.btn_editar.Size = new System.Drawing.Size(49, 41);
+            this.btn_editar.TabIndex = 13;
+            this.btn_editar.UseVisualStyleBackColor = true;
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.BackgroundImage = global::Capa_Vista_DispoInve.Properties.Resources.guardar;
+            this.btn_guardar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_guardar.Location = new System.Drawing.Point(655, 21);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(49, 41);
+            this.btn_guardar.TabIndex = 12;
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Capa_Vista_DispoInve.Properties.Resources.buscar;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(985, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 38);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Frm_DispoInve
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1054, 574);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_ayuda);
+            this.Controls.Add(this.btn_imprimir);
+            this.Controls.Add(this.btn_actualizar);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.btn_editar);
+            this.Controls.Add(this.btn_guardar);
             this.Controls.Add(this.cmbTipo);
             this.Controls.Add(this.cmbAlmacen);
             this.Controls.Add(this.txtBusqueda);
@@ -150,7 +270,7 @@ namespace Capa_Vista_DispoInve
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Frm_DispoInve";
-            this.Text = "Frm_DispoInve";
+            this.Text = "Disponibilidad de Inventario - 731 ";
             this.Load += new System.EventHandler(this.Frm_DispoInve_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvInventario)).EndInit();
             this.ResumeLayout(false);
@@ -170,5 +290,15 @@ namespace Capa_Vista_DispoInve
         private System.Windows.Forms.TextBox txtBusqueda;
         private System.Windows.Forms.ComboBox cmbAlmacen;
         private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.Button btn_editar;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_actualizar;
+        private System.Windows.Forms.Button btn_imprimir;
+        private System.Windows.Forms.Button btn_ayuda;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
