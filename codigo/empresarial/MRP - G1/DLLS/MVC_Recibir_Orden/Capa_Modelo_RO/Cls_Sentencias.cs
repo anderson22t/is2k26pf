2c5280b0038315ea76bf6ad9ab384d33b0f01354
Leticia Sontay <lsontayc1@miumg.edu.gt>
2026-04-28 08:37:19 -0600

    Se agregan sentencias para detalle de orden

--- codigo/empresarial/MRP - G1/DLLS/MVC_Recibir_Orden/Capa_Modelo_RO/Cls_Sentencias.cs
+++ codigo/empresarial/MRP - G1/DLLS/MVC_Recibir_Orden/Capa_Modelo_RO/Cls_Sentencias.cs
@@ -37,5 +37,51 @@ namespace Capa_Modelo_RO
 
             return dt;
         }
+
+        public DataTable ObtenerDetalleOrden(int idOrden)
+        {
+            DataTable dt = new DataTable();
+            using (OdbcConnection conn = conexion.AbrirConexion())
+            {
+                string query = @"
+            SELECT 
+                d.Pk_Id_Detalle,
+                m.Codigo_Material                       AS Material,
+                m.Nombre_Material                       AS Descripcion,
+                u.Abreviatura_Unidad_Medida             AS [Unidad Medida],
+                d.Cantidad_Solicitada                   AS [Cantidad Solicitada]
+            FROM Tbl_Orden_Recibida_Detalle d
+            INNER JOIN Tbl_Materiales m 
+                ON d.Fk_Id_Material = m.Pk_Id_Materiales
+            INNER JOIN Tbl_Unidad_Medida u
+                ON m.Fk_Id_Unidad_Medida = u.Pk_Id_Unidad_Medida
+            WHERE d.Fk_Id_Orden_Recibida = ?";
+
+                OdbcCommand cmd = new OdbcCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", idOrden);
+
+                OdbcDataAdapter da = new OdbcDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+        public DataTable ObtenerOrdenesCombo()
+        {
+            DataTable dt = new DataTable();
+            using (OdbcConnection conn = conexion.AbrirConexion())
+            {
+                string query = @"
+            SELECT 
+                o.Pk_Id_Orden_Recibida,
+                o.Id_Externo_Logistica AS Orden
+            FROM Tbl_Orden_Recibida o
+            ORDER BY o.Id_Externo_Logistica";
+
+                OdbcDataAdapter da = new OdbcDataAdapter(query, conn);
+                da.Fill(dt);
+            }
+            return dt;
+        }
     }
 }
