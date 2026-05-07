94456bdd99121e045d13ce6d1e4859792c65ffed
Natareno2124 <kevinnajera24@gmail.com>
2026-05-07 16:19:11 -0600

    Métodos para cargar orden en el combobox, se añade pestaña de consumo de materiales

--- codigo/empresarial/MRP - G1/DLLS/MVC_Produccion/Capa_Vista_Prod/Frm_Produccion.cs
+++ codigo/empresarial/MRP - G1/DLLS/MVC_Produccion/Capa_Vista_Prod/Frm_Produccion.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Capa_Controlador_Prod;
 
 namespace Capa_Vista_Prod
 {
     public partial class Frm_Produccion : Form
     {
+        Cls_Controlador_Prod controlador = new Cls_Controlador_Prod();
         public Frm_Produccion()
         {
             InitializeComponent();
+            this.Load += Frm_Prod_Load;
+            ToolTip tip = new ToolTip();
+            tip.IsBalloon = true;
+            tip.ToolTipTitle = "Búsqueda de Órdenes";
+            tip.SetToolTip(Cbo_Orden, "Seleccione o escriba el número de orden para realizar la implosión.");
+        }
+
+        private void Frm_Prod_Load(object sender, EventArgs e)
+        {
+            // Configurar DateTimePickers igual que Kevin
+            
+
+            ObtenerOrdenesRecibidas();
+           
+        }
+
+        private void ObtenerOrdenesRecibidas()
+        {
+            DataTable dt = controlador.ObtenerOrdenesRecibidas();
+            Cbo_Orden.DataSource = dt;
+            Cbo_Orden.DisplayMember = "No_Orden";
+            Cbo_Orden.ValueMember = "Pk_Id_Orden_Recibida";
+            Cbo_Orden.SelectedIndex = -1;
         }
     }
 }
